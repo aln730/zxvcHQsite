e805ed90f1be6897da19a28255cb486913f6f410
aln730 <arnsg730@proton.me>
2025-07-11 17:03:55 -0400

    fix

--- script.js
+++ script.js
@@ -11,7 +11,7 @@
       function drawMatrix() {
         ctx.fillStyle = 'rgba(0, 0, 0, 0.05)';
         ctx.fillRect(0, 0, canvas.width, canvas.height);
-        ctx.fillStyle = '#004400';
+        ctx.fillStyle = '#d7ff00';
         ctx.font = fontSize + 'px monospace';
     
         for (let i = 0; i < drops.length; i++) {
